8640de293ba253f57db2d697d9db4de5ed60e34a
Sean Gephardt <22643339+SeanGephardt1@users.noreply.github.com>
2021-11-03 11:44:57 -0700

    CHECKIN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import { RootDiv } from './styles.js';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App()
+{
+	const [ count, setCount ] = useState( 0 );
+	const [ itemText, setItemText ] = useState( '' );
+	const [ list, addToList ] = useState( [] );
+
+	function AddToList(ev)
+	{
+		//	console.debug( 'start', count, itemText.target.value, list);
+		//	NOT NEEDED setItemText( itemText.target.value );
+
+		let _c = count;
+		let _new = {
+			id: _c,
+			text: itemText.target.value
+		};
+		_c++;
+
+		setCount( _c );
+		addToList( [...list, _new] );
+
+		//	console.debug( 'end', count, list, _new );
+		return;
+	}
+
+	function DeleteTodoItem( ev )
+	{	//	console.debug( 'DeleteTodoItem', ev.target.value );
+		let _sliced_array = list.filter( ( item =>
+		{
+			return item.id !== parseInt(ev.target.value);
+		} ) );
+		addToList( [ ..._sliced_array] );
+		return;
+	};
+
+	return (
+		<RootDiv>
+			<header>Keller Williams React Demo</header>
+			<div>
+				<input type="text" onChange={ setItemText } />
+				<button onClick={ AddToList } >Add</button>
+			</div>
+			{
+				list.map( ( item, idx ) => (
+					<div key={ idx }>
+						<div>
+							{ item.id } :: { item.text }
+						</div>
+						<div>
+							<button onClick={ DeleteTodoItem } value={ item.id }>Delete</button>
+						</div>
+					</div>
+					) )
+			}
+		</RootDiv>
+	);
+};
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const RootDiv = styled.div`
+	font-family:arial;
+	text-align:center;
+	font-size:24px;
+`;
+
+export const Link = styled.div`
+	color:white;
+	text-align:center;
+`;
